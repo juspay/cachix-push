fbe9734921e19abc4830dc4ea425aaa82cf61991
Sridhar Ratnakumar <srid@srid.ca>
2024-09-02 18:11:30 -0400

    Add app description

--- flake-module.nix
+++ flake-module.nix
@@ -34,23 +34,26 @@ in
       };
 
       config = {
-        apps.cachix-push.program =
+        apps.cachix-push =
           let
             inherit (config.cachix-push) package pathsToCache cacheName;
           in
-          pkgs.writeShellApplication {
-            name = "cachix-push-and-pin";
-            meta.description = ''
-              Run `cachix push` & `cachix pin` for each path in `cache-pins.pathsToCache`
-            '';
-            runtimeInputs = [ package ];
-            text = ''
-              set -x
-              cachix push ${cacheName} ${lib.concatStringsSep " " (lib.attrValues pathsToCache)}
-              ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: path: ''
-                cachix pin ${cacheName} ${name}-${system} ${path}
-              '') pathsToCache)}
-            '';
+          rec {
+            inherit (program) meta;
+            program = pkgs.writeShellApplication {
+              name = "cachix-push-and-pin";
+              meta.description = ''
+                Run `cachix push` & `cachix pin` for each path in `cache-pins.pathsToCache`
+              '';
+              runtimeInputs = [ package ];
+              text = ''
+                set -x
+                cachix push ${cacheName} ${lib.concatStringsSep " " (lib.attrValues pathsToCache)}
+                ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: path: ''
+                  cachix pin ${cacheName} ${name}-${system} ${path}
+                '') pathsToCache)}
+              '';
+            };
           };
       };
     });
